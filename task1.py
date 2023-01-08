7d91697395430669b625d9126efc9583116819bc
Semen Oskolkov <oskolkovsemen@yandex.ru>
2023-01-09 04:15:09 +0600

    File with task 1

---
+++ task1.py
@@ -0,0 +1,55 @@
+'''
+1. def to_camel_case(text):
+   return re.split('_|-', text)[1] + ''.join(word.title() for word in re.split('_|-', "text")[1::])
+'''
+
+
+def to_camel_case(text):
+    return re.split('_|-', text)[1] + ''.join(word.title() for word in re.split('_|-', "text")[1::])  # Ошибка отступов
+
+
+'''
+2. class SingletonMeta(type):
+
+_instances = {}
+
+    def *str*(cls, *args, **kwargs):
+        if cls in cls._instances:
+            instance = super().*call*(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+'''
+
+
+class SingletonMeta(type):
+    _instances = {}  # Ошибка отступа
+
+    def __str__(cls, *args, **kwargs):
+        if cls in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+'''
+3. count_bits = lambda: bin(n).count('1')
+'''
+
+count_bits = lambda n: bin(n).count('1')  # в lambda не указан аргумент
+
+'''
+4. def digital_root(n):
+    return if n < 10 n else digital_root(summ(map(int,str(n))))
+'''
+
+
+def digital_root(n):
+    return n if n < 10 else (
+        digital_root(summ(map(int, str(n)))))  # Неверно поставлено n (нужно после return, а не после 10
+
+
+'''
+5. even_or_odd = lambda number: "Even" if % 2 == 0 else "Odd"
+'''
+
+even_or_odd = lambda number: "Even" if number % 2 == 0 else "Odd"  # Аргумент не указан в выражении
